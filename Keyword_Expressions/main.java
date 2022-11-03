7d3a2725b642612e4b23d18a7fa1da6a163f2bc4
drkevinfriday <kevinfriday87@gmail.com>
2022-11-02 22:34:38 -0500

    commit test

---
+++ Keyword_Expressions/main.java
@@ -0,0 +1,7 @@
+package Keyword_Expressions;
+
+class test {
+ public static void main(String[] args){
+    System.out.println("Subscribe now!");
+ }
+}
---
+++ Statements/main.java
@@ -0,0 +1,12 @@
+package Statements;
+
+public class main {
+    public static void main(String[] args) {
+        int myVar=50;
+        myVar++;
+        myVar--;
+        System.out.println("test");
+        System.out.println(myVar);
+        
+    }
+}
